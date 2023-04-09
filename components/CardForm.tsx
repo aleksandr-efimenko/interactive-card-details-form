c121c46407dc8a3b1be004678833d8bc679d1393
androidblog <androidblog@yandex.ru>
2023-04-09 16:48:22 +0400

    add form inputs

--- components/CardForm.tsx
+++ components/CardForm.tsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React from "react";
 
 export default function CardForm() {
   return (
     <div className="">
-        <form className="">
-          <label className=" block" htmlFor="cardholder-name">
-            Cardholder Name
-            <input className="block" type="text" id="cardholder-name"  placeholder='e.g. Jane Appleseed' />
-          </label>
-          <label className="block" htmlFor="card-number">
-            Card Number
-            <input className='' type="text" id="card-number" />
-          </label>
-          <div className="flex"></div>
-          <label htmlFor=""></label>
-          <input type="submit" className=""/>
-        </form>
-      </div>
-  )
+      <form className="">
+        <label className=" block" htmlFor="cardholder-name">
+          Cardholder Name
+          <input
+            className="block"
+            type="text"
+            id="cardholder-name"
+            placeholder="e.g. Jane Appleseed"
+          />
+        </label>
+        <label className="block" htmlFor="card-number">
+          Card Number
+          <input
+            className="block"
+            type="text"
+            id="card-number"
+            placeholder="e.g. 1234 5678 9123 0000"
+          />
+        </label>
+        <div className="flex">
+          <div className="flex-1">
+            <label className="block" htmlFor="expiration-date">
+              <div className="block">
+                Exp. Date (MM/YY)
+                <input
+                  className=""
+                  type="text"
+                  id="expiration-date-mm"
+                  placeholder="MM"
+                />
+                <input
+                  className=""
+                  type="text"
+                  id="expiration-date-yy"
+                  placeholder="YY"
+                />
+              </div>
+            </label>
+          </div>
+        </div>
+        <label htmlFor=""></label>
+        <input type="submit" className="" />
+      </form>
+    </div>
+  );
 }
