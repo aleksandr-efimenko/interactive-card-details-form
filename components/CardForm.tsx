9e816a42bb3b742cc2fb91f49a83f9c6cf267a99
androidblog <androidblog@yandex.ru>
2023-04-09 14:17:25 +0400

    create project

---
+++ components/CardForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function CardForm() {
+  return (
+    <div className="">
+        <form className="">
+          <label className="block" htmlFor="cardholder-name">
+            Cardholder Name
+            <input className="i" type="text" id="cardholder-name"  placeholder='e.g. Jane Appleseed' />
+          </label>
+          <label className="block" htmlFor="card-number">
+            Card Number
+            <input className="" type="text" id="card-number" />
+          </label>
+          <div className="flex"></div>
+          <label htmlFor=""></label>
+          <input type="submit" className=""/>
+        </form>
+      </div>
+  )
+}
