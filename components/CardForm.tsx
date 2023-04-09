77b3728fecbabf0d346410b43bae8d2a7d087bc8
androidblog <androidblog@yandex.ru>
2023-04-09 16:40:03 +0400

    create background

--- components/CardForm.tsx
+++ components/CardForm.tsx
@@ -4,13 +4,13 @@ export default function CardForm() {
   return (
     <div className="">
         <form className="">
-          <label className="block" htmlFor="cardholder-name">
+          <label className=" block" htmlFor="cardholder-name">
             Cardholder Name
-            <input className="i" type="text" id="cardholder-name"  placeholder='e.g. Jane Appleseed' />
+            <input className="block" type="text" id="cardholder-name"  placeholder='e.g. Jane Appleseed' />
           </label>
           <label className="block" htmlFor="card-number">
             Card Number
-            <input className="" type="text" id="card-number" />
+            <input className='' type="text" id="card-number" />
           </label>
           <div className="flex"></div>
           <label htmlFor=""></label>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import CardForm from "@/components/CardForm";
+import bgMain from "@/public/images/bg-main-desktop.png";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Space_Grotesk({ subsets: ["latin"], weight: ["500"] });
 
 export default function Home() {
   return (
-    <main className="min-height-full">
-      <div className=""></div>
-      <CardForm />
+    <main style={inter.style} className="min-h-full ">
+      <div className="grid grid-cols-6">
+        <div className="col-start-1 col-end-3 relative min-h-screen">
+          <Image
+            src={bgMain}
+            alt="background"
+            fill
+            style={{
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+          />
+        </div>
+        <div className="flex absolute center">
+            
+        </div>
+        <div className="col-start-3 col-end-7 flex justify-center items-center">
+          <CardForm />
+        </div>
+      </div>
     </main>
   );
 }
