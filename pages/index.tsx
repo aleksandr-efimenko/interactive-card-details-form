1cd38a6d372cbcc48dfb683167eebc9820db77d9
androidblog <androidblog@yandex.ru>
2023-04-10 12:33:18 +0400

    rebuild background

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,34 @@ import Image from "next/image";
 import { Space_Grotesk } from "next/font/google";
 import CardForm from "@/components/CardForm";
 import bgMain from "@/public/images/bg-main-desktop.png";
+import cardFront from "@/public/images/bg-card-front.png";
+import cardBack from "@/public/images/bg-card-back.png";
 
 const inter = Space_Grotesk({ subsets: ["latin"], weight: ["500"] });
 
 export default function Home() {
   return (
-    <main style={inter.style} className="min-h-full ">
-      <div className="grid grid-cols-6">
-        <div className="col-start-1 col-end-3 relative min-h-screen">
-          <Image
-            src={bgMain}
-            alt="background"
-            fill
-            style={{
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          />
-        </div>
-        <div className="flex absolute center">
-            
+    <main style={inter.style} className="min-h-full">
+      <div className=" top-0 left-0 absolute w-1/3 min-h-full z-0">
+        <Image
+          src={bgMain}
+          alt="background"
+          fill
+          style={{
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        />
+      </div>
+      <div className=" min-h-full grid grid-cols-6 z-10 relative">
+        <div className="flex flex-col col-start-2 col-end-3">
+          <div className="">
+            <Image src={cardFront} alt="card front" />
+          </div>
+          <div>
+            <Image src={cardBack} alt="card back" />
+          </div>
         </div>
         <div className="col-start-3 col-end-7 flex justify-center items-center">
           <CardForm />
