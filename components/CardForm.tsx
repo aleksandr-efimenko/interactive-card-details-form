02ed3a5ef6f0ece9afa02d6526eebbf37f74dc8d
androidblog <androidblog@yandex.ru>
2023-04-12 18:11:52 +0400

    refactor functions for card check

--- components/CardForm.tsx
+++ components/CardForm.tsx
@@ -3,7 +3,14 @@ import React, { useEffect } from "react";
 import { SpinnerIcon } from "./SpinnerIcon";
 import { CardHolderNameInput } from "./CardHolderNameInput";
 import { CardNumberInput } from "./CardNumberInput";
-import { luhnCheck } from "@/utils/creditCardInputCheck";
+import {
+  checkCardHolder,
+  checkCardNumber,
+  checkCardExpiryMM,
+  checkCardExpiryYY,
+  checkCardExpiryDate,
+  checkCardCVV,
+} from "@/utils/creditCardInputCheck";
 import { CardExpiryInput } from "./CardExpiryInput";
 import { CardCvcInput } from "./CardCvcInput";
 
@@ -28,156 +35,67 @@ export default function CardForm({
   const [cardCvvError, setCardCvvError] = React.useState("");
 
   const [isLoading, setIsLoading] = React.useState(false);
+
   const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     let errors = false;
+
     // check card holder name
-    switch (true) {
-      case !cardHolder.trim(): {
-        setCardHolderError("Can’t be blank");
-        errors = true;
-        break;
-      }
-      case cardHolder.replace(/\s/g, "").length < 3: {
-        setCardHolderError("Card holder name must be at least 3 characters");
-        errors = true;
-        break;
-      }
-      case cardHolder.length > 30: {
-        setCardHolderError("Card holder name must be less than 30 characters");
-        errors = true;
-        break;
-      }
-      default: {
-        setCardHolderError("");
-        break;
-      }
+    const verifyCardHolder = checkCardHolder(cardHolder);
+    if (verifyCardHolder) {
+      setCardHolderError(verifyCardHolder);
+      errors = true;
+    } else {
+      setCardHolderError("");
     }
-    console.log(cardNumber.length);
+
     // check card number
-    switch (true) {
-      case !cardNumber: {
-        setCardNumberError("Can’t be blank");
-        errors = true;
-        break;
-      }
-      // check if 16 digits without spaces
-      case cardNumber.replace(/\s/g, "").length < 16: {
-        setCardNumberError("Card number must be 16 digits");
-        errors = true;
-        break;
-      }
-      // check if numbers only and spaces
-      case !/^[0-9\s]+$/.test(cardNumber): {
-        setCardNumberError("Wrong format, numbers only");
-        errors = true;
-        break;
-      }
-      //remove spaces
-      case !luhnCheck(cardNumber.replace(/\s/g, "")): {
-        setCardNumberError(
-          "Card number is not valid, try for test 4539670073707590"
-        );
-        errors = true;
-        break;
-      }
-      default: {
-        setCardNumberError("");
-        break;
-      }
+    const verifyCardNumber = checkCardNumber(cardNumber);
+    if (verifyCardNumber) {
+      setCardNumberError(verifyCardNumber);
+      errors = true;
+    } else {
+      setCardNumberError("");
     }
 
     // check card expiry mm
-    switch (true) {
-      case !cardExpiryMM: {
-        setCardExpiryMMError("Can’t be blank");
-        errors = true;
-        break;
-      }
-      case cardExpiryMM.length < 2: {
-        setCardExpiryMMError("MM must be 2 digits");
-        errors = true;
-        break;
-      }
-      // check if numbers only
-      case !/^[0-9]+$/.test(cardExpiryMM): {
-        setCardExpiryMMError("Wrong format, numbers only");
-        errors = true;
-        break;
-      }
-      case parseInt(cardExpiryMM) > 12: {
-        setCardExpiryMMError("Month must be less than 12");
-        errors = true;
-        break;
-      }
-      default: {
-        setCardExpiryMMError("");
-        break;
-      }
+    const verifyCardExpiryMM = checkCardExpiryMM(cardExpiryMM);
+    if (verifyCardExpiryMM) {
+      setCardExpiryMMError(verifyCardExpiryMM);
+      errors = true;
+    } else {
+      setCardExpiryMMError("");
     }
 
     // check card expiry yy
-    switch (true) {
-      case !cardExpiryYY: {
-        setCardExpiryYYError("Can’t be blank");
-        errors = true;
-        break;
-      }
-      case cardExpiryYY.length < 2: {
-        setCardExpiryYYError("YY must be 2 digits");
-        errors = true;
-        break;
-      }
-      // check if numbers only
-      case !/^[0-9]+$/.test(cardExpiryYY): {
-        setCardExpiryYYError("Wrong format, numbers only");
-        errors = true;
-        break;
-      }
-      default: {
-        setCardExpiryYYError("");
-        break;
-      }
+    const verifyCardExpiryYY = checkCardExpiryYY(cardExpiryYY);
+    if (verifyCardExpiryYY) {
+      setCardExpiryYYError(verifyCardExpiryYY);
+      errors = true;
+    } else {
+      setCardExpiryYYError("");
     }
 
     //check card expiry
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth() + 1;
-    const expiryYear = parseInt(`20${cardExpiryYY}`);
-    const expiryMonth = parseInt(cardExpiryMM);
-    if (expiryYear < currentYear) {
-      setCardExpiryMMError("Card is expired");
-      setCardExpiryYYError("Card is expired");
+    const verifyCardExpiryDate = checkCardExpiryDate(
+      cardExpiryMM,
+      cardExpiryYY
+    );
+    if (verifyCardExpiryDate) {
+      setCardExpiryMM(verifyCardExpiryDate);
+      setCardExpiryYY(verifyCardExpiryDate);
       errors = true;
-    }
-    if (expiryYear === currentYear && expiryMonth < currentMonth) {
-      setCardExpiryMMError("Card is expired");
-      setCardExpiryYYError("Card is expired");
-      errors = true;
+    } else {
+      setCardExpiryResultError("");
     }
 
     // check card cvv
-    switch (true) {
-      case !cardCvv: {
-        setCardCvvError("Can’t be blank");
-        errors = true;
-        break;
-      }
-      case cardCvv.length < 3: {
-        setCardCvvError("CVV must be 3 digits");
-        errors = true;
-        break;
-      }
-      // check if numbers only
-      case !/^[0-9]+$/.test(cardCvv): {
-        setCardCvvError("Wrong format, numbers only");
-        errors = true;
-        break;
-      }
-      default: {
-        setCardCvvError("");
-        break;
-      }
+    const verifyCardCvv = checkCardCVV(cardCvv);
+    if (verifyCardCvv) {
+      setCardCvvError(verifyCardCvv);
+      errors = true;
+    } else {
+      setCardCvvError("");
     }
 
     if (errors) {
--- utils/creditCardInputCheck.ts
+++ utils/creditCardInputCheck.ts
@@ -1,4 +1,45 @@
 // This function is used to check if the card number is valid
+export function checkCardHolder(cardHolder: string) {
+  switch (true) {
+    case !cardHolder.trim(): {
+      return "Can’t be blank";
+    }
+    case cardHolder.replace(/\s/g, "").length < 3: {
+      return "Card holder name must be at least 3 characters";
+    }
+    case cardHolder.length > 30: {
+      return "Card holder name must be less than 30 characters";
+    }
+    default: {
+      return "";
+    }
+  }
+}
+
+export function checkCardNumber(cardNumber: string) {
+  switch (true) {
+    case !cardNumber.trim(): {
+      return "Can’t be blank";
+    }
+    // check if 16 digits without spaces
+    case cardNumber.replace(/\s/g, "").length < 16: {
+      return "Card number must be 16 digits";
+    }
+    // check if numbers only and spaces
+    case !/^[0-9\s]+$/.test(cardNumber): {
+      return "Wrong format, numbers only";
+    }
+    //remove spaces
+    case !luhnCheck(cardNumber.replace(/\s/g, "")): {
+      return "Card number is not valid, try for test 4539670073707590";
+    }
+    default: {
+      return "";
+    }
+  }
+}
+
+// This function is used to check if the card number is valid
 export function luhnCheck(cardNumber: string): boolean {
   let sum = 0;
   let digit;
@@ -19,4 +60,79 @@ export function luhnCheck(cardNumber: string): boolean {
   }
 
   return sum % 10 === 0;
+}
+
+// check card expiry mm
+export function checkCardExpiryMM(cardExpiryMM: string) {
+  switch (true) {
+    case !cardExpiryMM.trim(): {
+      return "Can’t be blank";
+    }
+    case cardExpiryMM.length < 2: {
+      return "Card expiry month must be 2 digits";
+    }
+    case !/^[0-9]+$/.test(cardExpiryMM): {
+      return "Wrong format, numbers only";
+    }
+    case parseInt(cardExpiryMM) > 12: {
+      return "Card expiry month must be less than 12";
+    }
+    default: {
+      return "";
+    }
+  }
+}
+
+// check card expiry yy
+export function checkCardExpiryYY(cardExpiryYY: string) {
+  switch (true) {
+    case !cardExpiryYY.trim(): {
+      return "Can’t be blank";
+    }
+    case cardExpiryYY.length < 2: {
+      return "Card expiry year must be 2 digits";
+    }
+    case !/^[0-9]+$/.test(cardExpiryYY): {
+      return "Wrong format, numbers only";
+    }
+    default: {
+      return "";
+    }
+  }
+}
+
+//check card expiry date
+export function checkCardExpiryDate(
+  cardExpiryMM: string,
+  cardExpiryYY: string
+) {
+  //check card expiry
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const expiryYear = parseInt(`20${cardExpiryYY}`);
+  const expiryMonth = parseInt(cardExpiryMM);
+  if (expiryYear < currentYear) {
+    return "Card is expired";
+  }
+  if (expiryYear === currentYear && expiryMonth < currentMonth) {
+    return "Card is expired";
+  }
+}
+
+// check card cvv
+export function checkCardCVV(cardCVV: string) {
+  switch (true) {
+    case !cardCVV.trim(): {
+      return "Can’t be blank";
+    }
+    case cardCVV.length < 3: {
+      return "Card CVV must be 3 digits";
+    }
+    case !/^[0-9]+$/.test(cardCVV): {
+      return "Wrong format, numbers only";
+    }
+    default: {
+      return "";
+    }
+  }
 }
